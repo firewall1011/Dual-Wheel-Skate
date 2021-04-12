d6613e8034eb2b5c7b3fc8dc6d50ead21b0d18bc
Tiago Marino <tmsilva11@gmail.com>
2021-04-11 22:15:03 -0300

    Main menu with transition animation

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using NaughtyAttributes;
+
+[CreateAssetMenu]
+public class SceneController : ScriptableObject
+{
+    [Scene]
+    public int gameScene;
+    [Scene]
+    public int creditsScene;
+    [Scene]
+    public int menuScene;
+
+    public void LoadMenuScene() => SceneManager.LoadScene(menuScene, LoadSceneMode.Single);
+    public void LoadCreditsScene() => SceneManager.LoadScene(creditsScene, LoadSceneMode.Single);
+    public void LoadGameScene() => SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+}
